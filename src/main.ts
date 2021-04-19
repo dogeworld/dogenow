e5cb12168b80a6cf93d4ff9fd814de5641733f6d
dogeworld <dogeworlddog@gmail.com>
2021-04-19 12:36:01 +0900

    add huobi

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import superagent from "superagent";
 
     let upbitDisplay: DomNode;
     let gopaxDisplay: DomNode;
+    let huobiDisplay: DomNode;
     let binanceDisplay: DomNode;
 
     el(".content",
         el("h1", "도지나우 - 도지코인 현재가격 모든 거래소 정보"),
         upbitDisplay = el(".price-display", el("span.exchange", "업비트")),
         gopaxDisplay = el(".price-display", el("span.exchange", "고팍스")),
+        huobiDisplay = el(".price-display", el("span.exchange", "후오비 코리아")),
         binanceDisplay = el(".price-display", el("span.exchange", "Binance")),
         el("a.madeby-button", { href: "https://dogeworld.dog" },
             el("img", { src: "/images/madeby.png", width: "300" }),
@@ -26,6 +28,10 @@ import superagent from "superagent";
         (async () => {
             const gopaxData = await superagent.get("https://dogeworlddog.wixsite.com/apis/_functions/gopax");
             gopaxDisplay.empty().append(el("span.exchange", "고팍스"), el("span.price", `${JSON.parse(gopaxData.body.text).price} 원`));
+        })();
+        (async () => {
+            const huobiData = await superagent.get("https://api-cloud.huobi.co.kr/market/history/trade?symbol=dogekrw");
+            huobiDisplay.empty().append(el("span.exchange", "후오비 코리아"), el("span.price", `${huobiData.body.data[0].data[0].price} 원`));
         })();
         (async () => {
             const binanceData = await superagent.get("https://sochain.com/api/v2/get_price/DOGE/USD");
